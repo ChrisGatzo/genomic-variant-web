21932987a2fe62500469a26929baa1d375095d41
Chris Gatzonis <chris.gatzonis@todaytix.com>
2018-04-11 20:12:51 -0400

    add search button with click handler

--- src/GeneSearch.js
+++ src/GeneSearch.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import { Search } from './components';
 
 export default class GeneSearch extends Component {
+  state = {
+    searchTerm: '',
+  };
+
+  onChange = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+
+  handleSearchClick = () => {
+    console.log('perform searcb');
+  };
+
   render() {
+    const { searchTerm } = this.state;
+
     return (
       <div>
-        <Search />
+        <Search
+          onChange={this.onChange}
+          onClick={this.handleSearchClick}
+          value={searchTerm}
+        />
       </div>
     );
   }
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Search({ placeholder }) {
-  return <input placeholder={placeholder} />;
+export default function Search({ onChange, onClick, placeholder, searchTerm }) {
+  return (
+    <div>
+      <input onChange={onChange} placeholder={placeholder} value={searchTerm} />
+      <button onClick={onClick}>Search</button>
+    </div>
+  );
 }
 
 Search.propTypes = {
