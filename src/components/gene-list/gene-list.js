640ce1cf180cfb7b26c7eef68c4fd5b863eebc4c
Chris Gatzonis <chris.gatzonis@todaytix.com>
2018-04-14 21:08:43 -0400

    add basic mobile styles

--- src/components/gene-list/gene-list.js
+++ src/components/gene-list/gene-list.js
@@ -7,42 +7,44 @@ export default function GeneList({ genes }) {
   const renderProp = value => value || '-';
 
   return (
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th>Gene</th>
-          <th>Nucleotide Change</th>
-          <th>Protein Change</th>
-          <th>Alias</th>
-          <th>Region</th>
-          <th>Reported Classification</th>
-          <th>Last Evaluated</th>
-          <th>Last Updated</th>
-          <th>More Info</th>
-        </tr>
-      </thead>
-      <tbody>
-        {genes.map((g, i) => (
-          <tr key={i}>
-            <td>{renderProp(g.gene)}</td>
-            <td>
-              <ExpandingColumn values={g.otherMappings} />
-            </td>
-            <td>{renderProp(g.proteinChange)}</td>
-            <td>{renderProp(g.alias)}</td>
-            <td>{renderProp(g.region)}</td>
-            <td>{renderProp(g.reportedClassification)}</td>
-            <td>{renderProp(g.lastEvaluated)}</td>
-            <td>{renderProp(g.lastUpdated)}</td>
-            <td>
-              <a href={g.url} target="_blank">
-                {g.source}
-              </a>
-            </td>
+    <div className={styles.tableWrapper}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Gene</th>
+            <th>Nucleotide Change</th>
+            <th>Protein Change</th>
+            <th>Alias</th>
+            <th>Region</th>
+            <th>Reported Classification</th>
+            <th>Last Evaluated</th>
+            <th>Last Updated</th>
+            <th>More Info</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {genes.map((g, i) => (
+            <tr key={i}>
+              <td>{renderProp(g.gene)}</td>
+              <td>
+                <ExpandingColumn values={g.otherMappings} />
+              </td>
+              <td>{renderProp(g.proteinChange)}</td>
+              <td>{renderProp(g.alias)}</td>
+              <td>{renderProp(g.region)}</td>
+              <td>{renderProp(g.reportedClassification)}</td>
+              <td>{renderProp(g.lastEvaluated)}</td>
+              <td>{renderProp(g.lastUpdated)}</td>
+              <td>
+                <a href={g.url} target="_blank">
+                  {g.source}
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
