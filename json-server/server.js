0b45fd82e1878ca25066142279e9908bdbfafa5c
Chris Gatzonis <chris.gatzonis@invitae.com>
2020-03-29 16:13:27 -0400

    fixes mock server route

--- json-server/server.js
+++ json-server/server.js
@@ -1,29 +1,21 @@
 'use strict';
-const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const rewriteRules = require('./routes.json')
-const app = express();
-const data = require('./data/data.js')
-
 const bodyParser = require('body-parser');
-// var jsonServer = require('json-server');
+var jsonServer = require('json-server');
+const data = require('./data/data.js')
+const rewriteRules = require('./routes.json');
 
-// app.use(jsonServer.rewriter(rewriteRules))
-// app.use('/api', jsonServer.router(data()));
+const app = jsonServer.create()
+const router = jsonServer.router(data())
+const middlewares = jsonServer.defaults()
 
-const router = express.Router();
-router.get('/', (req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<h1>Hello from Express.js!</h1>');
-    res.end();
-});
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+app.use(middlewares)
+app.use('/.netlify/functions/server', jsonServer.rewriter(rewriteRules))
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.use(jsonServer.rewriter(rewriteRules))
+app.use(router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
